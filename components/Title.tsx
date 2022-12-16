8bca5ad258f0c4673456f258708dc92f80710c55
idoncev <idoncev@gmail.com>
2022-12-16 15:35:19 +0100

    extend title component

--- components/Title.tsx
+++ components/Title.tsx
@@ -4,13 +4,26 @@ import clsx from 'clsx'
 import { PropsWithChildren } from 'react'
 import styles from './Title.module.scss'
 
+const TitleSizeVariant = {
+    "BASE": "base",
+    "LARGE": "large",
+    "XL": "xl",
+} as const
+
+type TitleSizeVariant = typeof TitleSizeVariant[keyof typeof TitleSizeVariant]
+
 type TitleProps = {
-    disabled?: boolean
+    disabled?: boolean,
+    size?: TitleSizeVariant,
+    as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span" | "p",
+    bold?: boolean,
+    medium?: boolean,
+    className?:string
 }
 
-export const Title = ({ children, disabled }: PropsWithChildren<TitleProps>) => {
-  
-    return <h2 className={clsx(styles.title, disabled && styles.disabled)}>{children}</h2>
+
+export const Title = ({ children, disabled, size="base", as:Component="h2", bold, medium, className }: PropsWithChildren<TitleProps>) => {
+    return <Component className={clsx(styles.title, className, disabled && styles.disabled, styles[size], bold && styles.bold || medium && styles.medium)}>{children}</Component>
 }
 
 export default Title
