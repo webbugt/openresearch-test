1ed741218bc8e15ddd86d4b091b5091de92359df
idoncev <idoncev@gmail.com>
2022-12-16 13:09:25 +0100

    finsih ProfileImage for now

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -4,19 +4,18 @@ import styles from './ProfileImage.module.scss'
 import Image from 'next/image'
 export const ProfileImage = ({ image, name, size = 40, hasBorder = false }:{image?:string, name?: string, size?: number, hasBorder?: boolean}) => {
 
-    const parsedSize = hasBorder ? size - 8 : size
     const shortName = name?.split(" ").map(s => s?.charAt(0).toUpperCase()).join("")
     return <div 
         className={styles.container}
         style={{
-            width: parsedSize,
-            height: parsedSize,
-            borderRadius: parsedSize / 2,
+            width: size,
+            height: size,
+            borderRadius: size / 2,
             border: hasBorder ? `4px solid #F6F8FA` : 'none',
             overflow: "hidden"
         }}
     >
-        {image ? <Image src={image} title={name} alt={name ||""} width={size} height={size} /> : <span>{shortName || "NN"}</span>}
+        {image ? <Image src={image} title={name} alt={name || ""}  fill /> : <div className={styles.name}><span>{shortName || "NN"}</span></div>}
     </div>
 
 }
