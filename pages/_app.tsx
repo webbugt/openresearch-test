2cdbb709dc23cf3a462fc5331f75624c04322ff2
idoncev <idoncev@gmail.com>
2022-12-16 12:31:11 +0100

    implement next/font and montserrat

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+import { Montserrat } from '@next/font/google'
+
+const montserrat = Montserrat({
+  weight: ['400','500','700'],
+  subsets: ['latin','latin-ext'],
+})
 
 import { QueryClient, QueryClientProvider } from 'react-query'
 
@@ -8,7 +15,8 @@ const queryClient = new QueryClient()
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <main className={montserrat.className} >
+        <Component {...pageProps} /></main>
     </QueryClientProvider>
   )
 }
